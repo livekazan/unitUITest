a461ad4819726157867ef72ea11479c29a470daf
livekazan <d3monStration>
2015-05-28 15:01:32 +0300

    presentation

--- src/test/uitest/LoginPage.java
+++ src/test/uitest/LoginPage.java
@@ -9,7 +9,7 @@ public class LoginPage {
 
     @Test
     public void visibleButtonLogin(){
-        open("/unitTest/login.action");
+        open("/unitUITest/login");
         $(By.name("login")).setValue("user");
         $(By.name("password")).setValue("1").pressEnter();
         $(By.id("errorMsg")).shouldHave((text("Поля незаполнены")));
